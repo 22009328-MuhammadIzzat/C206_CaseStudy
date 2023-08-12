9fde287d04203b25646d24203a7ec7f546e257bb
22026515 <22026515@192.168.1.2>
2023-08-12 23:32:00 +0800

    Finished codes for Vendor menus; Add, view, delete menus

---
+++ C206_CaseStudy/src/VendorMenu.java
@@ -0,0 +1,36 @@
+/**
+	 * I declare that this code was written by me.
+	 * I will not copy or allow others to copy my code.
+	 * I understand that copying code is considered as plagiarism.
+	 *
+	 * vino, 10 Aug 2023 11:03:25 pm
+	 */
+
+	/**
+	 * @author vino
+	 *
+	 */
+public class VendorMenu {
+	private String menuCode;
+	private String menuName;
+		
+	public VendorMenu(String menuCode, String menuName) {
+		this.menuCode = menuCode;
+		this.menuName = menuName;
+		}
+	
+	public String toString() {
+		String itemInfo =  String.format("%-30s %-10s\n", menuCode, menuName);
+		return itemInfo;
+		}
+
+		public String getMenuCode() {
+			return menuCode;
+		}
+		
+		public String getMenuName() {
+			return menuName;
+		}
+	}
+
+
